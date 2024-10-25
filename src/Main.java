26e2b16dded039b2b3ad43f0a19ca8e04d9ee246
MAITRIYA SRAMANER <maitriya@outlook.fr>
2024-10-25 15:36:46 +0200

    first exo

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,8 +7,19 @@ public class Main {
         JFrame window = new JFrame("test");
         window.setSize(300,150);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.add(new JLabel("hello"));
-        window.add(new JLabel("World"));
+        JPanel mainPanel = new JPanel();
+
+        mainPanel.add(new JLabel("Welcome :"));
+        mainPanel.add(new JButton("I am First"));
+        mainPanel.add(new JButton("I am Second"));
+        mainPanel.add(new JLabel("OutPut"));
+
+        JButton b = new JButton("click me");
+
+        b.addActionListener(new ButtonAction());
+        mainPanel.add(b);
+
+        window.add(mainPanel);
 
         window.setVisible(true);
     }
